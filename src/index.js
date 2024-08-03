a953b6189d4bea9a01e28646706c738bbf14452d
Sunnt Kumar <sunnydip3889@gmail.com>
2024-08-04 00:50:28 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -2,24 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./service/emailService');
+// const { sendBasicEmail } = require('./service/emailService');
+
+const TicketController = require('./controllers/ticketController');
+
+const jobs = require('./utils/cronJob');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
-
-        // sendBasicEmail(
-        //     'flightreminderservice@gmail.com',
-        //     'ravishkumar5731@gmail.com',
-        //     'Hello',
-        //     'Do bhai dono tabahi'
-        // );
+        jobs();
+        
     });
 
 }
 
 setupAndStartServer();
+
